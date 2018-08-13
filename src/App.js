7c9f8234c7076e91686aadebefb278427ac1d171
Gladys Ng <glng7121@gmail.com>
2018-08-13 14:50:14 -0700

    basic styles for Editor and RunManager components

--- src/App.js
+++ src/App.js
@@ -41,10 +41,10 @@ function RunButton(props) {
 
 class App extends Component {
   state = {
-    currentBaseWorkout: App.generateInitWorkout(), //workout_test,
-    exercise: null,
-    breakTime: new Time(0, null), 
-    isRunning: false
+    currentBaseWorkout: workout_test, //App.generateInitWorkout(), //workout_test,
+    exercise: 'blah', //todo: should be null,
+    breakTime: new Time(0, 3), //todo: sec should be null
+    isRunning: false //todo: should be false
   }
 
   static generateInitWorkout = () => {
--- src/RunManager.js
+++ src/RunManager.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Countdown from './Countdown.js';
 import ParsedBreakTime from './ParsedBreakTime.js';
+import './RunManager.css';
+import './workoutDisplay.css';
 
 class RunManager extends Component {
     state = {
@@ -29,27 +31,59 @@ class RunManager extends Component {
     render() {
       const { isBreakTime, currSetIndex } = this.state;
       return (
-        <div> 
-          Break time: <ParsedBreakTime breakTime={this.props.breakTime} />
-          <table>
+        <div id='runManagerComponent'> 
+          <table className='workout-fields'>
             <tbody>
-              {this.props.baseWorkout.map((set, i) => 
-                  <tr key={set.key}> 
-                    <td> {`${i+1})`} </td>
-                    <td>
-                      {`${set.reps} ${this.props.exercise}`} 
-                    </td>
-                    { i === currSetIndex?
-                      <td>
-                        <button disabled={isBreakTime} onClick={this.toggleBreakTime}> End This Set </button> 
-                      </td> 
-                      : null }
-                    { i === currSetIndex && isBreakTime? 
-                      <td>
-                        <Countdown breakTime={this.props.breakTime} endBreak={this.endBreak} />
-                      </td> 
-                      : null }
-                  </tr> )}
+              <tr>
+                <td>
+                  <table className='setup-fields'>
+                    <tbody>
+                      <tr>
+                        <td> Exercise: </td>
+                        <td> 
+                            {this.props.exercise}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td> Break time: </td>
+                        <td> <ParsedBreakTime breakTime={this.props.breakTime} /> </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <table className='set-fields'>
+                      <thead>
+                        <tr>
+                          <th colSpan='4'> Sets </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        
+                      {this.props.baseWorkout.map((set, i) => 
+                          <tr key={set.key}>
+                            <td> {`${i+1})`} </td>
+                            <td>
+                                {set.reps}
+                            </td>
+                            { i === currSetIndex?
+                              <td>
+                                <button disabled={isBreakTime} onClick={this.toggleBreakTime}> End This Set </button> 
+                              </td> 
+                              : null }
+                            { i === currSetIndex && isBreakTime? 
+                              <td>
+                                <Countdown breakTime={this.props.breakTime} endBreak={this.endBreak} />
+                              </td> 
+                              : null }
+                          </tr>
+                      )}
+                      </tbody>
+                  </table>
+                </td>
+              </tr>
             </tbody>
           </table>
         </div>
