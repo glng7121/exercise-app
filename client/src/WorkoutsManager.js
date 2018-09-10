17f3f1bc696f7083358b36a014243db6f704db15
Gladys Ng <glng7121@gmail.com>
2018-09-09 18:30:54 -0700

    now tracking WorkoutsManager files. oops

---
+++ client/src/WorkoutsManager.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './WorkoutsManager.css';
+
+class WorkoutsManager extends Component {
+  render() {
+      return (
+        <div id='workoutsManagerComponent'>
+          <div className='button-menu'>
+            <button onClick={this.props.addEmptyWorkout}> New </button>
+            <button onClick={this.props.deleteCurrWorkout}> Delete </button>
+          </div>
+          <select id='workoutSelector' onChange={this.props.switchWorkouts} value={this.props.currWorkoutId}>
+          {Array.from(this.props.workouts.entries()).map((pair) => {
+            const key = pair[0], workout = pair[1];
+            return <option key={key} value={key}>{workout.name? workout.name : '(invalid name)'}</option>
+          })}
+        </select>
+        </div>
+      )
+  }
+}
+
+export default WorkoutsManager;
